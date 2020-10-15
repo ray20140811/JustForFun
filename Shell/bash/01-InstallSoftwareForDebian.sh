a56ef245abacc79771df7c1b3bd6c194ee1d7bb0
Ray Kuo <ray20200720@gmail.com>
2020-10-15 12:41:45 +0800

    update

--- Shell/bash/01-InstallSoftwareForDebian.sh
+++ Shell/bash/01-InstallSoftwareForDebian.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-sudo apt update
-sudo apt install zsh
-sudo apt install curl
-sudo apt install wget
+#sudo apt update
+#sudo apt install 
+
+#sudo apt install curl
+#sudo apt install wget
+#sudo apt install tree
+#sudo apt install zsh
+#sudo apt install vim
+#sudo apt install emacs
