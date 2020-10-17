d0b0967a8b7186990d3fd615e5037cc200eeaf22
ray <ray@Trisquel-8.0>
2020-10-17 11:13:23 +0800

    Add Trisquel

---
+++ LinuxDistribution/Trisquel/install_sofware.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+##sudo apt install 
+
+# Source Version Control
+#sudo apt install git
+
+# Text Editor
+#sudo apt install ne
+#sudo apt install geany
+#sudo apt install vim 
+#sudo apt install emacs
+
+## doom emacs
+#git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+#~/.emacs.d/bin/doom install
+
+# Web Browser
+#sudo apt install w3m
+#sudo apt install wget
+
+# Shell
+## zsh
+#sudo apt install zsh
+
+## oh-my-zsh
+#sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"")"
+
+
+#sudo apt install tree
+
+#sudo apt install tmux
+
+#sudo apt install npm
+
+#sudo apt install nodejs-legacy
+
+# MicroController
+#sudo apt install arduino
+
+
+
