3812e5b8accb0d1c1d576b4fe5c33feb8e82cef9
Ray Kuo <ray20140811@gmail.com>
2022-03-09 16:35:17 +0800

    create java

---
+++ Pages/Java/ex03/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] argv) {
+        Product product = new Product();
+        product.setName("Car");
+        System.out.println("This name of this product is " + product.getName());
+    }
+}
---
+++ Pages/Java/ex03/Product.java
@@ -0,0 +1,10 @@
+class Product {
+     String name;
+     double price;
+     String getName() {
+          return this.name;
+     }
+     void setName(String name) {
+          this.name = name;
+     }
+}
