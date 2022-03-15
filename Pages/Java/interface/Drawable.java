cb9ff832155e57cb60f619917fcce7767decb676
ray20140811 <ray20140811@gmail.com>
2022-03-15 10:35:06 +0800

    update java.html and create interface

---
+++ Pages/Java/interface/Drawable.java
@@ -0,0 +1,4 @@
+interface Drawable {
+    double PI = 3.14;
+    void draw();
+}
---
+++ Pages/Java/interface/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rec = new Rectangle();
+        rec.draw();
+    }
+}
