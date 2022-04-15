9cc717716981d1a5624878b58ae79d1d482bd850
Ray Kuo <ray20200720@gmail.com>
2022-04-15 23:40:31 +0800

    add init.el & init.el-bak (Linux Mint)

---
+++ Software/Emacs/init.el
@@ -0,0 +1,41 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (misterioso)))
+ '(package-selected-packages (quote (general))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'general)
+(general-define-key
+:keymaps 'override
+:prefix "SPC"
+"SPC" 'execute-extended-command
+;; file
+"ff" 'find-file
+
+;; dired
+;"dj" dired-jump
+"bb" 'switch-to-buffer
+"bB" 'switch-to-buffer-other-window
+"kb" 'kill-buffer
+"-" 'split-window-horizontally
+"/" 'split-window-vertically
+)
