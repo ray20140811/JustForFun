91d3cc1d9444fd7c8c6557bbdcb795d9ddac883c
Ray.Kuo <ray20200720@gmail.com>
2022-05-02 00:07:22 +0800

    [ADD] 03-elisp-guide & 04-AnTroductionToProgramming

---
+++ Software/Emacs/04-AnIntroductionToProgramming/elisp.el
@@ -0,0 +1,17 @@
+;; 2  Practicing Evaluation
+
+;; How to Evaluate
+;; C-x C-e
+
+;; 2.1 Buffer Names
+(buffer-name)
+
+(buffer-file-name)
+
+(buffer-name) "*scratch"
+
+(current-buffer)
+
+(other-buffer)
+
+
