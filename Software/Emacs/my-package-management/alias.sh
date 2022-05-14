3a79ce1e3f0bdf4b13eb36809122ea27d95ca2f2
Ray.Kuo <ray20200720@gmail.com>
2022-05-14 16:01:17 +0800

    update LearningGNUEmacs.org & create alias.sh

---
+++ Software/Emacs/my-package-management/alias.sh
@@ -0,0 +1,3 @@
+alias demacs="emacs -q -l ~/Projects/MyLabs/emacs/nano-emacs/nano.el -dark"
+alias emacs-nw="emacs -q -l ~/Projects/MyLabs/emacs/nano-emacs/nano.el -nw -dark"
+alias nemacs="emacs -q -l ~/Projects/MyLabs/emacs/nano-emacs/nano.el"
